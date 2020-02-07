b9214874a3d0686cdd41893ba586f5b5b1b64b52
wangdong <305306733@qq.com>
2020-02-07 18:43:50 +0800

    java多线程编程实战指南 清单1-9

---
+++ src/main/java/com/wangdong/multithreadprogram/shizhanzhinan/FileDownloaderApp.java
@@ -0,0 +1,47 @@
+package com.wangdong.multithreadprogram.shizhanzhinan;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.FileOutputStream;
+import java.net.URL;
+
+/**
+ * @author wangdong
+ */
+@Slf4j
+public class FileDownloaderApp {
+
+    public static void main(String[] args) {
+        Thread downLoaderThread;
+        for (String url : args) {
+            downLoaderThread = new Thread(new FileDownloader(url));
+            downLoaderThread.start();
+        }
+    }
+
+    static class FileDownloader implements Runnable {
+        private final String fileUrl;
+
+        public FileDownloader(String fileUrl) {
+            this.fileUrl = fileUrl;
+        }
+
+        @Override
+        public void run() {
+            log.info("Downloading from {}", fileUrl);
+            String fileBaseName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+            try {
+                URL url = new URL(fileUrl);
+                String localFileName = "C:\\新建文件夹" + fileBaseName;
+                log.info("save to {}", localFileName);
+                downloadFile(url, new FileOutputStream(localFileName), 1024);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void downloadFile(URL url, FileOutputStream fileOutputStream, int bufSize) throws Exception {
+            //-----略
+        }
+    }
+}

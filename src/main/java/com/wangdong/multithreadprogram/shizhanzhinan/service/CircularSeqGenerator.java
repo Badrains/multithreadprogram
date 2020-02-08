20b44306e3a702eaa469a1403adc49237703dbff
wangdong <305306733@qq.com>
2020-02-08 23:33:32 +0800

    java多线程编程实战指南 清单2-1

---
+++ src/main/java/com/wangdong/multithreadprogram/shizhanzhinan/service/CircularSeqGenerator.java
@@ -0,0 +1,13 @@
+package com.wangdong.multithreadprogram.shizhanzhinan.service;
+
+/**
+ * @author wangdong
+ */
+public interface CircularSeqGenerator {
+
+    /**
+     * 获取下一个序列号
+     * @return
+     */
+    short nextSequence();
+}

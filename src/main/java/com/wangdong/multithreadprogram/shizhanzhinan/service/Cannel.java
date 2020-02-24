dc70c4e4d2d9c3ad62b0f28d14a73f6c0d8cdd23
wangdong <305306733@qq.com>
2020-02-24 17:47:38 +0800

    java多线程编程实战指南 清单5-10

---
+++ src/main/java/com/wangdong/multithreadprogram/shizhanzhinan/service/Cannel.java
@@ -0,0 +1,22 @@
+package com.wangdong.multithreadprogram.shizhanzhinan.service;
+
+/**
+ * @description: 5-10
+ * @author: wangdong
+ * @date: 2020/2/24 17:45
+ */
+public interface Cannel<P> {
+    /**
+     * 往传输通道中存入一个产品
+     * @param product
+     * @throws InterruptedException
+     */
+    void put(P product) throws InterruptedException;
+
+    /**
+     * 冲传输通道中取一个产品
+     * @return
+     * @throws InterruptedException
+     */
+    P take() throws InterruptedException;
+}
